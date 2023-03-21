0ff077f3c56e760cd6c15d1d4bfb98521a29b98a
argenismiranda <argenismiranda@gmail.com>
2023-03-21 15:34:25 -0700

    programming-functions

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-int main() {
-    int a = 1, b = 2, c, i;
-    printf("%d, %d, ", a, b);
-    for (i = 3; i <= 50; i++) {
-        c = a + b;
-        printf("%d", c);
-        if (i < 50) {
-            printf(", ");
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n = 50; // number of Fibonacci numbers to print
+    long int fib1 = 1, fib2 = 2, next; // initialize the first two Fibonacci numbers
+    
+    printf("%ld, %ld, ", fib1, fib2); // print the first two Fibonacci numbers
+    
+    for (int i = 3; i <= n; i++) {
+        next = fib1 + fib2; // calculate the next Fibonacci number
+        printf("%ld", next); // print the next Fibonacci number
+        if (i < n) {
+            printf(", "); // print a comma and space after each number (except the last)
         }
-        a = b;
-        b = c;
+        fib1 = fib2; // update the previous two Fibonacci numbers
+        fib2 = next;
     }
-    printf("\n");
+    printf("\n"); // print a newline character at the end
+    
     return 0;
 }
