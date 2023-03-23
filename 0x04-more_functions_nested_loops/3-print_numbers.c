bf6bcf5e8dce835429e1f4dc19b87c9a3e6d51e4
argenismiranda <argenismiranda@gmail.com>
2023-03-23 08:21:53 -0700

    programming-more-functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+followed by a new line.print_numbers - print 0123456789
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
