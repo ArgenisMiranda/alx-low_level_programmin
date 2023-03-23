5202f32b085cf1e291056f67304e17924ea1e540
argenismiranda <argenismiranda@gmail.com>
2023-03-23 08:42:22 -0700

    programming-more-functions

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * Return: a square made of '#'
+ */
+void print_square(int size)
+{
+	int co, ro;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= size; co++)
+		{
+			_putchar('#');
+			for (ro = 2; ro <= size; ro++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
