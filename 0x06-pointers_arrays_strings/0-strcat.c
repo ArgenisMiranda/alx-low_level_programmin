7fd84db577bb5db5b08ec57101153bbdc6cf5b86
argenismiranda <argenismiranda@gmail.com>
2023-03-29 08:24:51 -0700

    programming-pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,14 @@
 #include "main.h"
-
-
-
 char *_strcat(char *dest, char *src)
 {
-	int index_01 = 0;
-	int index_02 = 0;
+	char *ptr = dest;
 
-	while (dest[index_01] != '\0')
-	{
-		index_01++;
-	}
-
-	while (src[index_02] != '\0')
-	{
-		dest[index_01 + index_02] = src[index_02];
-		index_02++;
-
-	}
-	dest[index_01 + index_02] = '\0';
+	while (*ptr != 0)
+		ptr++;
+	while (*src != 0)
+		*ptr++ = *src++;
+	*ptr = 0;
 
 	return (dest);
 }
+
