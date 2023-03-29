856cbe93366ebc533b6062080968b368f3b4e2ad
argenismiranda <argenismiranda@gmail.com>
2023-03-29 08:07:08 -0700

    programming-pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+char *_strcat(char *dest, char *src)
+{
+	int index_01 = 0;
+	int index_02 = 0;
+
+	while (dest[index_01] != '\0')
+	{
+		index_01++;
+	}
+
+	while (src[index_02] != '\0')
+	{
+		dest[index_01 + index_02] = src[index_02];
+		index_02++;
+
+	}
+	dest[index_01 + index_02] = '\0';
+
+	return (dest);
+}
