a112858bb52d44483f588bd3ac77d34e449fa4e8
argenismiranda <argenismiranda@gmail.com>
2023-03-29 08:44:07 -0700

    programming-pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -26,3 +26,25 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
+
+/**
+ * get_length - returns the length of a string
+ * @s: String whose length we wish to determine
+ *
+ * Return: Length of s
+ */
+int get_length(char *s)
+{
+	int length;
+	int i;
+
+	length = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		++length;
+		i++;
+	}
+
+	return (length);
+}
