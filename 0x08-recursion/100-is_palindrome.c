5f78da7a3c7a747622b45c61081151f7acb52905
argenismiranda <argenismiranda@gmail.com>
2023-04-05 08:07:46 -0700

    alx-programming-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,62 +1,49 @@
-#include <main.h>
-#include <stdio.h>
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
+#include "main.h"
 
-int find_pal(char *s, int f_i, int b_i);
-int count_string(char *s);
 /**
- * is_palindrome - checks if string is a palindrom
- * @s: string to check.
- *
- * Return: 1 if a string is a palindrome and 0 if not.
+ * length - is the length of the string
+ * @s: string used
+ * Return:Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int length(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i = i + length(s + 1);
+		return (i += 1);
+	}
+	return (0);
+}
+
+/**
+ * help - length of the string by index
+ * @i: integer
+ * @s: string
+ * Return: int value of the result
+ */
+int help(int i, char *s)
+{
+	if (*s)
+	{
+		if (*s != s[length(s) - i])
+		{
+			return (0);
+		}
+		return (help(i + 1, s + 1));
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - is current value equal to the previous value
+ * @s: string
+ * Return: boolean
  */
 int is_palindrome(char *s)
 {
-	int count;
+	int i = 1;
 
-	count = 0;
-	if (!(*s))
-		return (1);
-
-	count = count_string(s);
-	return (find_pal(s, 0, count - 1));
-}
-
-/**
- * find_pal - helper for is_palindrom
- * @s: string to check
- * @f_i: front index.
- * @b_i: back index.
- *
- * Return: 1 if a string is a palindrome and 0 if not.
- */
-int find_pal(char *s, int f_i, int b_i)
-{
-	/*printf("%c - %c : %d - %d\n", s[f_i], s[b_i], f_i, b_i); */
-	if (s[f_i] != s[b_i])
-		return (0);
-	if (s[f_i] == s[b_i] && f_i == b_i)
-		return (1);
-	else if (s[f_i] == s[b_i] && f_i > b_i)
-		return (1);
-	else
-		return (find_pal(s, f_i + 1, b_i - 1));
-}
-
-/**
- * count_string - count the string.
- * @s: string to count.
- *
- * Return: Int value of the length.
- */
-int count_string(char *s)
-{
-	if (*s)
-		return (count_string((s + 1)) + 1);
-	return (0);
+	return (help(i, s));
 }
