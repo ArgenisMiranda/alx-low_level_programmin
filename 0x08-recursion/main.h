0d2b3f99a0117e3011d7bfd864cf22486ad711b6
argenismiranda <argenismiranda@gmail.com>
2023-04-05 07:55:18 -0700

    alx-programming-recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,9 @@
- * Prototypes for Module 0x08
- */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -8,6 +12,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int _putchar(char c);
-int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
+#endif
