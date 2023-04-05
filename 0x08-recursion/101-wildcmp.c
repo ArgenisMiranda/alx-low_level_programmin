9e08aa24a4a5c5a864324f595d4ba4aa7f09efe2
argenismiranda <argenismiranda@gmail.com>
2023-04-05 08:41:27 -0700

    alx-programming-recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
+ * wildcmp - compares two strings and returns 1
  * compare_strings - Compares two strings to check if they are identical
+ * if the strings can be considered identical,
+ * otherwise return 0.
+ * @s1: the normal string
+ * @s2: the special string containing "*"
  *
- * @s1: First string to compare
- * @s2: Second string to compare
- *
- * Return: 1 if the strings are identical, otherwise 0
+ * Return: 1 if identical, else 0
  */
-
 int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0' && *s1 == '\0')
@@ -24,4 +25,3 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
 	return (0);
-}
