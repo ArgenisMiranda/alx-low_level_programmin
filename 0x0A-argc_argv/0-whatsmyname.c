98d56b52a993ec8604dbe23f1ded115ba146898c
Argenismiranda <argenismiranda@gmail.com>
2023-04-08 14:15:51 -0700

    programing-argc-argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.>
+#include <string.h>
+/**
+ * main - Entry point
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+	}
