39990d56c0aa3b7b33f59d68f203627cebd76b16
Argenismiranda <argenismiranda@gmail.com>
2023-04-08 15:28:09 -0700

    programing-argc-argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,7 +3,6 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- *
  * Return: the int converted from the string
  */
 int _atoi(char *s)
@@ -35,15 +34,15 @@ int _atoi(char *s)
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
-		}
+}
 		i++;
-	}
+}
 
 	if (f == 0)
 		return (0);
 
 	return (n);
-}
+	}
 
 /**
  * main - multiplies two numbers
