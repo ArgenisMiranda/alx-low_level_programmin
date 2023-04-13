793fee403dc725ccdc83e6d9d194d69280b539f9
argenismiranda <argenismiranda@gmail.com>
2023-04-13 07:11:14 -0700

    programming-more-malloc-free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Amount of memory to allocate
+ * Return: Pointer to the allocated memory
+ *         or exit with status 98 if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *str;
+
+	str = malloc(b);
+	if (str == NULL)
+		exit(98);
+	return (str);
+	}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Programming-more-amlloc-free
