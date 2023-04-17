ad66faa6f1e4064148aaa0a83304cc241c7f3003
argenismiranda <argenismiranda@gmail.com>
2023-04-17 13:04:06 -0700

    programming-structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,41 +14,34 @@ void _strcopy(char *dst, char *src)
 }
 
 /**
-	 * new_dog - creates a new dog
-	 * @name: name of the dog
-	 * @age: age of the dog
-	 * @owner: owner of the dog
-	 *
-	 * Return: pointer to the new dog (Success), NULL otherwise
-	 */
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	int len1, len2;
+	dog_t *nd;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
-
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	p = malloc(sizeof(dog_t));
+	if (nd == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->name == NULL)
-
-	free(dog);
-	return (NULL);
-
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
-	if (dog->owner == NULL)
-
-	free(dog);
-	free(dog->name);
+	if (name == NULL)
+	{
+		free(nd);
+		free(owner);
 		return (NULL);
-
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
-	dog->age = age;
-
-		return (dog);
 	}
+	if (owner == NULL)
+	{
+		free(nd);
+		free(name);
+		return (NULL);
+	}
+	nd->name = name;
+	nd->age = age;
+	nd->owner = owner;
+	return (nd);
+}
