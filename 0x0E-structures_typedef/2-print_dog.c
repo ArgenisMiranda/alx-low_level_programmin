3ba1176a15ce2ad95f67b4047c70e51e2d315549
argenismiranda <argenismiranda@gmail.com>
2023-04-17 11:59:29 -0700

    programming-structure

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * print_dog - prints a dog struct
+ * @d: pointer to the struct dog
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	char *name = d->name != NULL ? d->name : "(nil)";
+	float age = d->age;
+	char *breed = d->breed != NULL ? d->breed : "(nil)";
+
+	printf("Name: %s\nAge: %.6f\nBreed: %s\n", name, age, breed);
+}
