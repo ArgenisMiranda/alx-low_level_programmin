70b845707fa9b51e6ea4d67caaea83aa42dc3a3a
argenismiranda <argenismiranda@gmail.com>
2023-04-18 06:47:37 -0700

    programming-structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,21 +1,21 @@
 #include "dog.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * new_dog - entry point
  * @name: string from main, name of pet
  * @age: number from main, age of pet
- * @owner: string from main, owner of pet
+ * @owner: string  owner of pet
  * Return: p
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
-	/* reserving memory to struct*/
+
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
-	/* Cpunting name pointer*/
+
 	if (name == NULL)
 	{
 		free(p);
@@ -32,4 +32,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	p->age = age;
 	p->owner = owner;
 	return (p);
+}
+
+/**
+ * _strcopy - copy read only data to mutatable.
+ * @dst: pointer to copy char to.
+ * @src: read only data.
+ */
+void _strcopy(char *dst, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dst[i] = src[i];
+	dst[i] = '\0';
 }
