1664fc617bd3981fb49fcdd64ee3b2a34fc9728a
argenismiranda <argenismiranda@gmail.com>
2023-04-18 07:52:23 -0700

    programming-structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,16 +34,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 	return (p);
 }
 
-/**
- * _strcopy - copy read only data to mutatable.
- * @dst: pointer to copy char to.
- * @src: read only data.
- */
-void _strcopy(char *dst, char *src)
-{
-	int i;
-
-	for (i = 0; src[i]; i++)
-		dst[i] = src[i];
-	dst[i] = '\0';
-}
