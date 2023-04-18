90937744e2569587643307e3d72f2d29610b028e
argenismiranda <argenismiranda@gmail.com>
2023-04-18 07:50:59 -0700

    programming-structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (p == NULL)
 		return (NULL);
 	p->name = malloc(strlen(name) + 1);
-	if (name == NULL)
+	if (p->name == NULL)
 	{
 		free(p);
 		return (NULL);
@@ -32,4 +32,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	p->age = age;
 	strcpy(p->owner, owner);
 	return (p);
+}
+
+/**
+ * _strcopy - copy read only data to mutatable.
+ * @dst: pointer to copy char to.
+ * @src: read only data.
+ */
+void _strcopy(char *dst, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dst[i] = src[i];
+	dst[i] = '\0';
 }
