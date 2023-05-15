0c8aafc42d606e29b478a22931112c1c08b4028b
argenismiranda <argenismiranda@gmail.com>
2023-05-15 07:51:02 -0700

    bit-manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,25 @@
 #include "main.h"
 
 /**
- * print_binary - converts the decimal format number into binary
+ * print_binary - converts the decimal number into binary
  * @n: decimal number
  */
 
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
-	int flag = 0;
+	int temp;
+	static int count;
 
-	while (mask > 0)
+	if (n == 0 && count > 0)
+		return;
+	else if (n == 0)
 	{
-		if ((n & mask) == mask)
-		{
-			putchar('1');
-			flag = 1;
-		}
-		else if (flag == 1 || mask == 1)
-		{
-			putchar('0');
-		}
-
-		mask >>= 1;
+		_putchar('0');
+		return;
 	}
 
-	if (n == 0)
-		putchar('0');
+	temp = (n & 1);
+	count++;
+	print_binary(n >>= 1);
+	_putchar('0' + temp);
 }
