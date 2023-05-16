32099905a59f2771f7cfb2871551ed623ad086cf
argenismiranda <argenismiranda@gmail.com>
2023-05-16 09:05:19 -0700

    programming-singly-linked-lists

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Adds a new node at the end of a linked list.
+ * @head: A pointer to a pointer to the head of the linked list.
+ * @n: The value to be stored in the new node.
+ *
+ * Return: The address of the new element, or NULL.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = (NULL);
+
+	if (*head == NULL)
+	{
+		*head = (new_node);
+	}
+	else
+	{
+		current = (*head);
+
+		while (current->next != NULL)
+			current = current->next;
+
+		current->next = (new_node);
+	}
+
+	return (new_node);
+}
