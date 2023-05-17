8f4afb305cb24bea4e0bf46875a579402c6b80cd
argenismiranda <argenismiranda@gmail.com>
2023-05-17 12:10:51 -0700

    programming-i0-file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads text file and prints the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
- * Return: The actual number of letters read and printed. 0 if there's an error.
+ * Return: The number of letters read and printed. 0 if there's an error.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
